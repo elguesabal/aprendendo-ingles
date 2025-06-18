beb46a1e04f741584beaab0422fdacd9323b07ad
elguesabal <joseelguesabal@gmail.com>
2025-06-18 18:55:32 -0300

    teste render

--- resources/views/components/head-meta.blade.php
+++ resources/views/components/head-meta.blade.php
@@ -1,10 +1,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Documenttttt</title>
-<!-- @vite('resources/css/app.css') -->
-
 @env('local')
     @vite('resources/css/app.css')
 @else
-    <link rel="stylesheet" href="{{ asset('build/assets/app-CWVmMBLi.css') }}">
+    <link rel="stylesheet" href="{{ secure_asset('build/assets/app-CWVmMBLi.css') }}">
 @endenv
